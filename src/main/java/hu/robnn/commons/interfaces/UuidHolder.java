f0cc04a08bb48b5d75afcbde9f61f89c1c7ccf67
Kurovszky Robin <robin.kurovszky@gmail.com>
2018-07-30 11:02:15 +0200

    Repository: init

--- src/main/java/hu/robnn/commons/interfaces/UuidHolder.java
+++
@@ -1,9 +0,0 @@
-package hu.robnn.commons.interfaces;
-
-/**
- * UuidHolder for marking a class that it can be handled by the merger
- */
-public interface UuidHolder {
-    String getUuid();
-    void setUuid(String uuid);
-}
